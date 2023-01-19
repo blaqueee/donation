ddbe460adb56ca5bd522ed4d175e6c893a9b8694
Maksataiii <erkbvna@gmail.com>
2023-01-20 03:48:13 +0600

    change some code

--- src/main/java/edu/jundev/donation/controller/AuthController.java
+++ src/main/java/edu/jundev/donation/controller/AuthController.java
@@ -6,6 +6,8 @@ import edu.jundev.donation.dto.response.ResponseJwt;
 import edu.jundev.donation.dto.response.ResponseMessage;
 import edu.jundev.donation.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
@@ -15,11 +17,11 @@ import javax.validation.Valid;
 public class AuthController {
     private final UserService userService;
     @PostMapping("/login")
-    public ResponseJwt authenticateUser(@Valid @RequestBody LoginRequest requestLogin){
-        return userService.authenticateUser(requestLogin);
+    public ResponseEntity<ResponseJwt> authenticateUser(@Valid @RequestBody LoginRequest requestLogin){
+        return ResponseEntity.ok(userService.authenticateUser(requestLogin));
     }
     @PostMapping(path="/register")
-    public ResponseMessage registerUser(@Valid @RequestBody RegisterRequest requestSignup){
-        return userService.registerUser(requestSignup);
+    public ResponseEntity<ResponseMessage> registerUser(@Valid @RequestBody RegisterRequest requestSignup){
+        return new ResponseEntity<>(userService.registerUser(requestSignup), HttpStatus.OK);
     }
 }
